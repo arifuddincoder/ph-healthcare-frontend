0de5f9ae504025ee8b707e5c4ad3cce8d9705a28
arifuddincoder <arifuddincoder@gmail.com>
2026-04-13 17:21:22 +0600

    feat:add prefetch technic to fetched data

--- src/app/(commonLayout)/consultation/page.tsx
+++ src/app/(commonLayout)/consultation/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { getDoctors } from "./_actions";
+import DoctorsList from "@/components/modules/consultation/DoctorsList";
 
-const ConsultationPage = () => {
-	return <div>Consultation Page</div>;
+const ConsultationPage = async () => {
+	const queryClient = new QueryClient();
+
+	await queryClient.prefetchQuery({
+		queryKey: ["doctors"],
+		queryFn: getDoctors,
+	});
+	return (
+		// Neat! Serialization is now as easy as passing props.
+		// HydrationBoundary is a Client Component, so hydration will happen there.
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<DoctorsList />
+		</HydrationBoundary>
+	);
 };
 
 export default ConsultationPage;
